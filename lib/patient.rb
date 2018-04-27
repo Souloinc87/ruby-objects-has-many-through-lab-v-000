bdf37dd8008b1bff1900578dfa36d0da63bb14d5
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-27 01:41:34 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,9 +6,13 @@ class Patient
     @appointments = []
   end
 
+  def appointments
+    @appointments
+  end 
+
   def add_appointment(appointment)
     @appointments << appointment
-    appointment.patient 
+    appointment.patient
   end
 
 end
